574047ded268ab501cfec9de731a901a25666372
duffaldri <mdaffaaldriantama@gmail.com>
2022-10-27 22:41:40 +0700

    First commit

---
+++ resources/views/about-us.blade.php
@@ -0,0 +1,44 @@
+{{-- @dd($reviews) --}}
+
+@extends('layouts.main')
+@section('container')
+
+<div class="container col-md-8 pt-5 mb-5">
+    <div class="row">
+        <h1 class="display-4 fw-semibold">Siapa kami?</h1>
+        <p>
+            DAKos didirikan pada 27 Oktober 2010, Sejak saat itu DAKos telah berkembang
+     hingga menjadi aplikasi pencari kos terbaik di Indonesia. DAKos memanfaatkan teknologi dengan mengelola 
+     dan menyajikan daftar kos dengan penjelasan fasilitas secara terperinci dari setiap kos. Saat ini Kami memiliki
+     lebih dari 3 juta kamar kos yang tersebar di lebih dari 100 kota di seluruh Indonesia. DAKos berusaha untuk bisa
+     menyajikan data ketersediaan kamar yang akurat sehingga calon penghuni akan mendapat kemudahan dalam pencarian kos.
+        </p>
+    </div>
+    
+    <div class="row mt-3">
+        <h3 class="display-6 fw-semibold">Team Members</h3>
+        <section class=" text-center p-2 mb-2">
+            <img class="rounded-circle" src="/img/dup.png" alt="Daffa" width="170" />
+            <h2 class="">Muhammad Daffa Aldriantama</h2>
+            <p class="lead">5025201177 | Informatics ITS</p>
+        </section>
+
+        <section class=" text-center p-2 mb-3">
+            <img class="rounded-circle" src="/img/jul.jpg" alt="Julio" width="170" />
+            <h2 class="">Julio Geraldi Soeiono</h2>
+            <p class="lead">5025201079 | Informatics ITS</p>
+        </section>
+    </div>
+</div>
+
+
+{{-- <div class="container">
+    @foreach ($reviews as $review)
+    <h2>{{ $review["name"] }}</h2>
+    <h5>{{ $review["job"] }}</h5>
+    <p>{{ $review["body"] }}</p>
+    @endforeach
+</div> --}}
+  <!-- Carousel wrapper -->
+
+@endsection
